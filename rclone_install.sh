5f002f01d0ab6df5380106caf949bb12eadf508e
damithkothalawala <admin@drklk.org>
2023-04-13 19:10:37 +0800

    add rclone

--- rclone_install.sh
+++ rclone_install.sh
@@ -2,13 +2,14 @@
 sudo apt update
 sudo apt install gnupg -y
 sudo apt install rclone -y
+sudo apt install gnupg2 -y
 sudo wget https://github.com/damithkothalawala/migration-scripts/raw/main/rclone
 sudo mv rclone /usr/bin/rclone
 sudo chmod +x /usr/bin/rclone
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 sudo wget https://github.com/damithkothalawala/migration-scripts/raw/main/rclone.conf.gpg
 echo "Please use provided key"
-gpg rclone.conf.gpg
+gpg2 rclone.conf.gpg
 sudo mkdir -p /root/.config/rclone/
 sudo mv rclone.conf /root/.config/rclone/
 sudo chown root.root /root/.config/rclone/rclone.conf  
