3397cee882b555ae0d38170fb3547bc8abfd127e
damithkothalawala <admin@drklk.org>
2023-04-13 13:12:31 +0800

    add rclone

--- rclone_install.sh
+++ rclone_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt update
 sudo apt install rclone -y
-
+sudo wget https://github.com/damithkothalawala/migration-scripts/raw/main/rclone
+sudo mv rclone /usr/bin/rclone
+sudo chmod +x /usr/bin/rclone
